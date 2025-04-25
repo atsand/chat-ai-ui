8e4475b387b874e172833f615b7c3c3e37c3af2c
Aaron Sandusky <aaron@supplystream.io>
2025-04-25 11:27:58 -0600

    hotfix - update gPiniaUser return type

--- src/pinia/user/getters.ts
+++ src/pinia/user/getters.ts
@@ -3,7 +3,7 @@ import type { User } from "../../types/types";
 import { isObjectPlain } from "../../utils/utils";
 
 export const getters = {
-  gPiniaUser: (): User => {
+  gPiniaUser: (): User | {} => {
     const USER_STORE = useUserStore();
 
     return isObjectPlain(USER_STORE.user) ? USER_STORE.user : {};
